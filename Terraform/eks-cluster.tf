e6362fe72eadfbdb99f795e7dc8bc9f152c06901
cunananan <cunananan@gmail.com>
2022-04-28 10:22:40 -0500

    Add Terraform files

---
+++ Terraform/eks-cluster.tf
@@ -0,0 +1,54 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "18.20.5"
+  cluster_name    = local.cluster_name
+  cluster_version = local.cluster_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  self_managed_node_group_defaults = {
+    instance_type                          = "t2.medium"
+    update_launch_template_default_version = true
+  }
+
+  self_managed_node_groups = {
+    aqua = {
+      name         = "aqua"
+      public_ip    = true
+      max_size     = 5
+      desired_size = 3
+    }
+  }
+
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::${var.iam_id}:role/admin"
+      username = "admin"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::${var.iam_id}:user/archie"
+      username = "archie"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  tags = {
+    Environment = "Prod"
+    Terraform   = "true"
+  }
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.11.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.11.0"
     }
-     kubernetes = {
+    kubernetes = {
       version = "~> 2.10.0"
     }
     helm = {
@@ -14,12 +14,10 @@ terraform {
       source  = "hashicorp/random"
       version = "3.1.0"
     }
-
     local = {
       source  = "hashicorp/local"
       version = "2.1.0"
     }
-
     null = {
       source  = "hashicorp/null"
       version = "3.1.0"
@@ -35,7 +33,8 @@ provider "aws" {
 }
 
 locals {
-  cluster_name = "team-aqua-${random_string.suffix.result}"
+  cluster_name    = "team-aqua-${random_string.suffix.result}"
+  cluster_version = "1.22"
 }
 
 resource "random_string" "suffix" {
@@ -48,91 +47,4 @@ resource "aws_default_vpc" "default" {
     Name = "Default VPC"
   }
 }
-  
 
-provider "aws"{
-  region = "us-east-1"
-  access_key = ""
-  secret_key = ""
-}
-
-resource "aws_instance" "aws_instance" {
-  ami  = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
-
-    tags = {
-    Name = "terraform"
-  }
-
-  resource "aws"
-}
-
-
-
-//EKS
-
-
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "18.20.5"
-  cluster_name    = local.cluster_name
-  cluster_version = "1.20"
-  subnets         = module.vpc.private_subnets
-
-  vpc_id = module.vpc.vpc_id
-
-  workers_group_defaults = {
-    root_volume_type = "gp2"
-  }
-
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.medium"
-      additional_security_group_ids = [aws_security_group.allow_tls.id]
-      asg_desired_capacity          = 3
-    }
-  ]
-}
-
-data "aws_eks_cluster" "eks_cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "auth_cluster" {
-  name = module.eks.cluster_id
-}
-
-
-resource "aws_iam_role_policy_attachment" "" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.example.name
-}
-
-
-//RDS
-
-
-//S3
-resource "aws_s3_bucket" "s3" {
-  bucket = "bubble-bucket"
-
-  tags = {
-    Name        = "bubble-bucket"
-    Environment = "Prod"
-  }
-}
-
-resource "aws_s3_bucket_acl" "bubble-bucket" {
-  bucket = aws_s3_bucket.s3.id
-  acl    = "private"
-}
-
-
-//Kubernetes
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-}
